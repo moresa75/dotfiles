5c7c8b81a89671675fed62c3dcb49ffb7bc0d0ae
Reza Sajadiany <rezasjd@berkeley.edu>
2021-10-13 13:17:54 -0700

    emacs conf

---
+++ .emacs.d/transient/history.el
@@ -0,0 +1,11 @@
+((magit-am
+  ("--3way"))
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-gitignore nil)
+ (magit-merge nil)
+ (magit-show-refs nil)
+ (magit-status-jump nil))
